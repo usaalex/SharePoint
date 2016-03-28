5f27ac1d646bee646fd0f079c8f8a2aac1be411e
Alexander Kleshcheov <kleshcheovalex@live.com>
2016-03-28 14:21:35 +0300

    Structure changed. Again.

---
+++ SharePoint_Services/old/spjs.1.0.2.intellisense.js
@@ -0,0 +1,201 @@
+/* SPJS intellisense 1.0.2 */
+/* https://github.com/usaalex/SharePoint */
+/* © WM-FDH, 2016 */
+intellisense.annotate(SPJS, {
+    'applyPeoplePickerFix': function () {
+        /// <signature>
+        ///	<summary>Apply People Picker bug fix.</summary>
+        ///	<returns type='void' />
+        /// </signature>
+    },
+    'getListItems': function () {
+        /// <signature>
+        /// <summary>Get list items by CAML query (Async).<para>On success: Array of SP.ListItem.</para><para>On failure: SP.ClientRequestFailedEventArgs.</para></summary>
+        /// <param name='list' type='String'>List name.</param>
+        /// <param name='camlQuery' type='String' optional='true'>CAML Query.</param>
+        /// <param name='viewFields' type='Array' optional='true'>Array of internal fields names.</param>		
+        /// <param name='rootWeb' type='Boolean' optional='true'>Perform query on root web.</param>
+        /// <returns type="$.Deferred" />
+        /// </signature>
+        /// <signature>
+        /// <summary>Get list items by CAML query (Async).<para>On success: Array of SP.ListItem.</para><para>On failure: SP.ClientRequestFailedEventArgs.</para></summary>
+        /// <param name='list' type='SP.List'>SP.List object.</param>
+        /// <param name='camlQuery' type='String' optional='true'>CAML Query.</param>
+        /// <param name='viewFields' type='Array' optional='true'>Array of internal fields names.</param>		
+        /// <param name='rootWeb' type='Boolean' optional='true'>Perform query on root web.</param>
+        /// <returns type='$.Deferred' />
+        /// </signature>
+    },
+    'getListItemsByIds': function () {
+        /// <signature>
+        /// <summary>Get list items by ids (Async).<para>On success: Array of SP.ListItem.</para><para>On failure: SP.ClientRequestFailedEventArgs.</para></summary>
+        /// <param name='list' type='String'>List name.</param>
+        /// <param name='ids' type='Array'>Array of ids.</param>
+        /// <param name='viewFields' type='Array' optional='true'>Array of internal fields names.</param>		
+        /// <param name='rootWeb' type='Boolean' optional='true'>Perform query on root web.</param>
+        /// <returns type='$.Deferred' />
+        /// </signature>
+        /// <signature>
+        /// <summary>Get list items by ids (Async).<para>On success: Array of SP.ListItem.</para><para>On failure: SP.ClientRequestFailedEventArgs.</para></summary>
+        /// <param name='list' type='SP.List'>SP.List object.</param>
+        /// <param name='ids' type='Array'>Array of ids.</param>
+        /// <param name='viewFields' type='Array' optional='true'>Array of internal fields names.</param>		
+        /// <param name='rootWeb' type='Boolean' optional='true'>Perform query on root web.</param>
+        /// <returns type='$.Deferred' />
+        /// </signature>
+    },
+    'createListItems': function () {
+        /// <signature>
+        /// <summary>Create list items (Async).<para>On success: Array of SP.ListItem.</para><para>On failure: SP.ClientRequestFailedEventArgs.</para></summary>
+        /// <param name='list' type='String'>List name.</param>
+        /// <param name='newItems' type='Array'>Array of objects ({ fieldA: value, fieldB: value }, each object represent a single item).<para>Example: [{ Title: 'Salary 1', Employee_Salary: 199 }, { Title: 'Salary 2', Employee_Salary: 299 }]</para></param>
+        /// <param name='rootWeb' type='Boolean' optional='true'>Perform query on root web.</param>
+        /// <returns type='$.Deferred' />
+        /// </signature>
+        /// <signature>
+        /// <summary>Create list items (Async).<para>On success: Array of SP.ListItem.</para><para>On failure: SP.ClientRequestFailedEventArgs.</para></summary>
+        /// <param name='list' type='SP.List'>SP.List object.</param>
+        /// <param name='newItems' type='Array'>Array of objects ({ fieldA: value, fieldB: value }, each object represent a single item).<para>Example: [{ Title: 'Salary 1', Employee_Salary: 199 }, { Title: 'Salary 2', Employee_Salary: 299 }]</para></param>
+        /// <param name='rootWeb' type='Boolean' optional='true'>Perform query on root web.</param>
+        /// <returns type='$.Deferred' />
+        /// </signature>
+    },
+    'deleteListItems': function () {
+        /// <signature>
+        /// <summary>Delete list items (Async).<para>On success: void.</para><para>On failure: SP.ClientRequestFailedEventArgs.</para></summary>
+        /// <param name='list' type='String'>List name.</param>
+        /// <param name='camlQuery' type='String'>CAML Query.</param>
+        /// <param name='rootWeb' type='Boolean' optional='true'>Perform query on root web.</param>
+        /// <returns type='$.Deferred' />
+        /// </signature>
+        /// <signature>
+        /// <summary>Delete list items (Async).<para>On success: void.</para><para>On failure: SP.ClientRequestFailedEventArgs.</para></summary>
+        /// <param name='list' type='SP.List'>SP.List object.</param>
+        /// <param name='camlQuery' type='String'>CAML Query.</param>
+        /// <param name='rootWeb' type='Boolean' optional='true'>Perform query on root web.</param>
+        /// <returns type='$.Deferred' />
+        /// </signature>
+    },
+    'deleteListItemsByIds': function () {
+        /// <signature>
+        /// <summary>Delete list items by ids (Async).<para>On success: void.</para><para>On failure: SP.ClientRequestFailedEventArgs.</para></summary>
+        /// <param name='list' type='String'>List name.</param>
+        /// <param name='ids' type='Array'>Array of ids.</param>
+        /// <param name='rootWeb' type='Boolean' optional='true'>Perform query on root web.</param>
+        /// <returns type='$.Deferred' />
+        /// </signature>
+        /// <signature>
+        /// <summary>Delete list items by ids (Async).<para>On success: void.</para><para>On failure: SP.ClientRequestFailedEventArgs.</para></summary>
+        /// <param name='list' type='SP.List'>SP.List object.</param>
+        /// <param name='ids' type='Array'>Array of ids.</param>
+        /// <param name='rootWeb' type='Boolean' optional='true'>Perform query on root web.</param>
+        /// <returns type='$.Deferred' />
+        /// </signature>
+    },
+    'updateListItems': function () {
+        /// <signature>
+        /// <summary>Update list items (Async).<para>On success: Array of SP.ListItem.</para><para>On failure: SP.ClientRequestFailedEventArgs.</para></summary>
+        /// <param name='list' type='String'>List name.</param>
+        /// <param name='updatedItems' type='Array'>Array of objects ({ ID: value, fieldA: value, fieldB: value }, each object represent a single item).<para>Example: [{ ID: 1, Title: 'Salary 1', Employee_Salary: 199 }, { ID: 2, Title: 'Salary 2', Employee_Salary: 299 }]</para></param>
+        /// <param name='rootWeb' type='Boolean' optional='true'>Perform query on root web.</param>
+        /// <returns type='$.Deferred' />
+        /// </signature>
+        /// <signature>
+        /// <summary>Update list items (Async).<para>On success: Array of SP.ListItem.</para><para>On failure: SP.ClientRequestFailedEventArgs.</para></summary>
+        /// <param name='list' type='SP.List'>SP.List object.</param>
+        /// <param name='updatedItems' type='Array'>Array of objects ({ ID: value, fieldA: value, fieldB: value }, each object represent a single item).<para>Example: [{ ID: 1, Title: 'Salary 1', Employee_Salary: 199 }, { ID: 2, Title: 'Salary 2', Employee_Salary: 299 }]</para></param>
+        /// <param name='rootWeb' type='Boolean' optional='true'>Perform query on root web.</param>
+        /// <returns type='$.Deferred' />
+        /// </signature>
+    },
+
+    'ensureUser': function () {
+        /// <signature>
+        /// <summary>Checks whether the specified login name belongs to a valid user in the site (Async).<para>On success: SP.User.</para><para>On failure: SP.ClientRequestFailedEventArgs.</para></summary>
+        /// <param name='userName' type='String'>User internal name.</param>
+        /// <returns type='$.Deferred' />
+        /// </signature>
+    },
+    'getUserProfile': function () {
+        /// <signature>
+        /// <summary>Get User profile (Async).<para>On success: Array of SP.ListItem.</para><para>On failure: SP.ClientRequestFailedEventArgs.</para></summary>
+        /// <param name='ids' optional='true' type='Array'>Array of ids. If no ids provided - returns current's user profile.</param>
+        /// <param name='viewFields' type='Array' optional='true'>Array of internal fields names.</param>
+        /// <returns type='$.Deferred' />
+        /// </signature>
+    },
+
+    'getGUID': function () {
+        /// <signature>
+        /// <summary>Get list GUID (Async).<para>On success: String (GUID).</para><para>On failure: SP.ClientRequestFailedEventArgs.</para></summary>
+        /// <param name='list' type='String'>List name.</param>	
+        /// <param name='rootWeb' type='Boolean' optional='true'>Perform query on root web.</param>
+        /// <returns type='$.Deferred' />
+        /// </signature>
+        /// <signature>
+        /// <summary>Get list GUID (Async).<para>On success: String (GUID).</para><para>On failure: SP.ClientRequestFailedEventArgs.</para></summary>
+        /// <param name='list' type='SP.List'>SP.List object.</param>
+        /// <param name='rootWeb' type='Boolean' optional='true'>Perform query on root web.</param>
+        /// <returns type='$.Deferred' />
+        /// </signature>
+    },
+
+    'showSPAlert': function () {
+        /// <signature>
+        /// <summary>Show SP.UI.ModalDialog.<para>On success: Boolean (dialog result OK - true, Cancel - false).</para></summary>
+        /// <param name='message' type='String'>Dialog contents (text or HTML).</param>	
+        /// <param name='title' type='String' optional='true'>Dialog title.</param>
+        /// <returns type='$.Deferred' />
+        /// </signature>
+    },
+    'showSPConfirm': function () {
+        /// <signature>
+        /// <summary>Show SP.UI.ModalDialog with OK and Cancel buttons.<para>On success: Boolean (dialog result OK - true, Cancel - false).</para></summary>
+        /// <param name='message' type='String'>Dialog contents (text or HTML).</param>	
+        /// <param name='title' type='String' optional='true'>Dialog title.</param>
+        /// <returns type='$.Deferred' />
+        /// </signature>
+    },
+    'showSPError': function () {
+        /// <signature>
+        /// <summary>Show SP.UI.ModalDialog.<para>On success: Boolean (dialog result OK - true, Cancel - false).</para></summary>
+        /// <param name='message' type='String'>Dialog contents (text or HTML).</param>	
+        /// <param name='title' type='String' optional='true'>Dialog title.</param>
+        /// <returns type='$.Deferred' />
+        /// </signature>
+    },
+    'showSPPopup': function () {
+        /// <signature>
+        /// <summary>Show SP.UI.ModalDialog.<para>On success: Boolean (dialog result OK - true, Cancel - false).</para></summary>
+        /// <param name='url' type='String'>Dialog URL.</param>	
+        /// <param name='title' type='String' optional='true'>Dialog title.</param>
+        /// <param name='hideClose' type='Boolean' optional='true'>Hide close button.</param>
+        /// <returns type='$.Deferred' />
+        /// </signature>
+    },
+
+    'showSPWaitDialog': function () {
+        /// <signature>
+        /// <summary>Show SP.UI.ModalDialog.showWaitScreenWithNoClose.</summary>
+        /// <param name='message' type='String' optional='true'>Dialog contents (text or HTML).</param>	
+        /// <param name='title' type='String' optional='true'>Dialog title.</param>
+        /// <param name='timeout' type='Number' optional='true'>Amount of milliseconds before wait dialog closes automatically.</param>
+        /// <param name='height' type='Number' optional='true'>Dialog height.</param>
+        /// <param name='width' type='Number' optional='true'>Dialog width.</param>
+        /// <returns type='void' />
+        /// </signature>
+    },
+    'hideSPWaitDialog': function () {
+        /// <signature>
+        /// <summary>Hide SP.UI.ModalDialog.showWaitScreenWithNoClose.</summary>
+        /// <returns type='void' />
+        /// </signature>
+    },
+    'getEntitiesPeoplePicker': function () {
+        /// <signature>
+        ///	<summary>Get resolved entities (user logins) from People Picker control.</summary>
+        /// <param name='controlID' type='String'>People Picker control id.</param>	
+        ///	<returns type='Array' />
+        /// </signature>
+    },
+});
---
+++ SharePoint_Services/old/spjsq.1.0.0.intellisense.js
@@ -0,0 +1,233 @@
+/* SPJS intellisense 1.0.2 */
+/* https://github.com/usaalex/SharePoint */
+/* © WM-FDH, 2016 */
+intellisense.annotate(SPJS.Query, function () {
+    /// <signature>
+    /// <summary>CAML Query builder.</summary>                        
+    /// <returns type="SPJS.Query" />
+    /// </signature>
+});
+var _spjsqobj = SPJS.Query;
+SPJS.Query = function () {
+    var _object = new _spjsqobj();
+    intellisense.annotate(_object, {
+        'And': function () {
+            /// <signature>
+            /// <summary>Logical AND.</summary>                        
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+        },
+        'Or': function () {
+            /// <signature>
+            /// <summary>Logical OR.</summary>                        
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+        },
+        'Eq': function () {
+            /// <signature>
+            /// <summary>Equals to comparison operator.</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <param name='value' type='T'>Field value.</param>
+            /// <param name='valueType' type='String' optional='true'>Field value type.</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+            /// <signature>
+            /// <summary>Equals comparison operator.</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <param name='value' type='T'>Field value.</param>
+            /// <param name='lookup' type='Boolean' optional='true'>Field value type is lookup.</param>
+            /// <param name='lookupId' type='Boolean' optional='true'>Lookup field value by id. (Default = true).</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+        },
+        'Neq': function () {
+            /// <signature>
+            /// <summary>Not equals to comparison operator.</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <param name='value' type='T'>Field value.</param>
+            /// <param name='valueType' type='String' optional='true'>Field value type.</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+            /// <signature>
+            /// <summary>Not equals to comparison operator.</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <param name='value' type='T'>Field value.</param>
+            /// <param name='lookup' type='Boolean' optional='true'>Field value type is lookup.</param>
+            /// <param name='lookupId' type='Boolean' optional='true'>Lookup field value by id. (Default = true).</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+        },
+        'Gt': function () {
+            /// <signature>
+            /// <summary>Greater than comparison operator.</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <param name='value' type='T'>Field value.</param>
+            /// <param name='valueType' type='String' optional='true'>Field value type.</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+            /// <signature>
+            /// <summary>Greater than comparison operator.</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <param name='value' type='T'>Field value.</param>
+            /// <param name='lookup' type='Boolean' optional='true'>Field value type is lookup.</param>
+            /// <param name='lookupId' type='Boolean' optional='true'>Lookup field value by id. (Default = true).</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+        },
+        'Geq': function () {
+            /// <signature>
+            /// <summary>Greater or equals to comparison operator.</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <param name='value' type='T'>Field value.</param>
+            /// <param name='valueType' type='String' optional='true'>Field value type.</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+            /// <signature>
+            /// <summary>Greater or equals to comparison operator.</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <param name='value' type='T'>Field value.</param>
+            /// <param name='lookup' type='Boolean' optional='true'>Field value type is lookup.</param>
+            /// <param name='lookupId' type='Boolean' optional='true'>Lookup field value by id. (Default = true).</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+        },
+        'Lt': function () {
+            /// <signature>
+            /// <summary>Less than comparison operator.</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <param name='value' type='T'>Field value.</param>
+            /// <param name='valueType' type='String' optional='true'>Field value type.</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+            /// <signature>
+            /// <summary>Less than comparison operator.</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <param name='value' type='T'>Field value.</param>
+            /// <param name='lookup' type='Boolean' optional='true'>Field value type is lookup.</param>
+            /// <param name='lookupId' type='Boolean' optional='true'>Lookup field value by id. (Default = true).</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+        },
+        'Leq': function () {
+            /// <signature>
+            /// <summary>Less or equals to comparison operator.</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <param name='value' type='T'>Field value.</param>
+            /// <param name='valueType' type='String' optional='true'>Field value type.</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+            /// <signature>
+            /// <summary>Less or equals to comparison operator.</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <param name='value' type='T'>Field value.</param>
+            /// <param name='lookup' type='Boolean' optional='true'>Field value type is lookup.</param>
+            /// <param name='lookupId' type='Boolean' optional='true'>Lookup field value by id. (Default = true).</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+        },
+        'BeginsWith': function () {
+            /// <signature>
+            /// <summary>Begins with comparison operator. Searches for a string at the start of a column.</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <param name='value' type='T'>Field value.</param>
+            /// <param name='valueType' type='String' optional='true'>Field value type.</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+            /// <signature>
+            /// <summary>Begins with comparison operator. Searches for a string at the start of a column.</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <param name='value' type='T'>Field value.</param>
+            /// <param name='lookup' type='Boolean' optional='true'>Field value type is lookup.</param>
+            /// <param name='lookupId' type='Boolean' optional='true'>Lookup field value by id. (Default = true).</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+        },
+        'Contains': function () {
+            /// <signature>
+            /// <summary>Contains comparison operator. Searches for a string anywhere within a column.</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <param name='value' type='T'>Field value.</param>
+            /// <param name='valueType' type='String' optional='true'>Field value type.</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+            /// <signature>
+            /// <summary>Contains comparison operator. Searches for a string anywhere within a column.</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <param name='value' type='T'>Field value.</param>
+            /// <param name='lookup' type='Boolean' optional='true'>Field value type is lookup.</param>
+            /// <param name='lookupId' type='Boolean' optional='true'>Lookup field value by id. (Default = true).</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+        },
+        'In': function () {
+            /// <signature>
+            /// <summary>In comparison operator. Specifies whether the field value is equal to one of the values.</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <param name='values' type='Array'>Field values.</param>
+            /// <param name='valueType' type='String' optional='true'>Field value type.</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+            /// <signature>
+            /// <summary>In comparison operator. Specifies whether the field value is equal to one of the values.</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <param name='values' type='Array'>Field values.</param>
+            /// <param name='lookup' type='Boolean' optional='true'>Field value type is lookup.</param>
+            /// <param name='lookupId' type='Boolean' optional='true'>Lookup field value by id. (Default = true).</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+        },
+        'IsNull': function () {
+            /// <signature>
+            /// <summary>Used within a query to return items that are empty (Null).</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+        },
+        'IsNotNull': function () {
+            /// <signature>
+            /// <summary>Used within a query to return items that are not empty (Null).</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+        },
+        'OrderBy': function () {
+            /// <signature>
+            /// <summary>Determines the sort order for a query.</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <param name='descending' type='String' optional='true'>Descending order. (Default = false).</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+        },
+        'GroupBy': function () {
+            /// <signature>
+            /// <summary>Group the data returned through a query.</summary>    
+            /// <param name='fieldName' type='String'>Internal field name.</param>
+            /// <param name='collapse' type='Boolean' optional='true'>TRUE for the Group By section in the list view to be collapsed by default. (Default = false)</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+        },
+        'RowLimit': function () {
+            /// <signature>
+            /// <summary>Specifies maximum results count for a query.</summary>    
+            /// <param name='count' type='Number'>Maximum results count.</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+        },
+        'Scope': function () {
+            /// <signature>
+            /// <summary>Specifies view scope and additional view attributes.</summary>    
+            /// <param name='viewScope' type='String'>View scope and additional view attributes.</param>
+            /// <returns type="SPJS.Query" />
+            /// </signature>
+        },
+        'build': function () {
+            /// <signature>
+            /// <summary>Build CAML Query.</summary>
+            /// <returns type="String" />
+            /// </signature>
+        },
+    });
+
+    return _object;
+};
+intellisense.redirectDefinition(SPJS.Query, _spjsqobj);
